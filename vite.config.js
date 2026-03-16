aa043cebfdfc64ea81b4a3739a9b4bb48d6f8af0
marija kovacki <kurucevm@gmail.com>
2026-03-16 16:55:25 +0100

    Add blog page and text

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
+import { resolve } from 'path'
+import fs from 'fs'
+
+const textPages = {}
+
+fs.readdirSync('./texts').forEach((file) => {
+  if (file.endsWith('.html')) {
+    const name = file.replace('.html', '')
+    textPages[name] = resolve(__dirname, 'texts', file)
+  }
+})
+
 export default {
   build: {
     rollupOptions: {
       input: {
-        main: "index.html",
-        about: "about.html",
+        main: resolve(__dirname, 'index.html'),
+        blogPage: resolve(__dirname, 'blog.html'),
+        about: resolve(__dirname, 'about.html'),
+        ...textPages,
       },
     },
   },
-};
+}
